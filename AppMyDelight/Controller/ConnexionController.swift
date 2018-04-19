ed5636bd0d51dc5a8ad00bc64dc43533ce6645b8
vincent portier <scrapp3rz@gmail.com>
2018-04-19 21:11:50 +0200

    instanciation 1ere page

---
+++ AppMyDelight/Controller/ConnexionController.swift
@@ -0,0 +1,22 @@
+//
+//  ConnexionController.swift
+//  AppMyDelight
+//
+//  Created by vincent portier on 19/04/2018.
+//  Copyright © 2018 vincent portier. All rights reserved.
+//
+
+import UIKit
+
+class ConnexionController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+
+    }
+
+
+
+}
